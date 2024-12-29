32d28585ed15ede985eff2945aecd8b73dc67e75
Ali Jones <aliclara564@gmail.com>
2024-12-28 17:49:09 -0800

    making it green

--- testled.py
+++ testled.py
@@ -2,9 +2,9 @@ import board
 import neopixel
 import time
 
-pixels = neopixel.NeoPixel(board.D18, 150)
+pixels = neopixel.NeoPixel(board.D18, 150, brightness=0.2, auto_write=False)
 
-pixels[0] = (255, 0, 0)
+pixels.fill((0, 255, 0))
 pixels.show()
 time.sleep(5)
 
