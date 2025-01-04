bb3dce2d7a220af6a6d5c76a4675f114548363a5
Ali Jones <aliclara564@gmail.com>
2025-01-03 17:54:54 -0800

    adding led configuration script

--- testled.py
+++ testled.py
@@ -2,18 +2,38 @@
 # arrows on the strip mean something, fancy that.
 
 # Need some m-m jumper wires and we're otherwise ready to go. They're BRIGHT even at 20% power
-# Should also play around with how much voltage we need, this was ~8
+# Need 5V power
 
 import board
 import neopixel
 import time
 
 pixels = neopixel.NeoPixel(board.D18, 150, brightness=0.2, auto_write=False)
+exit_loop = False
 
-pixels.fill((0, 255, 0))
-pixels.show()
-time.sleep(5)
+print("LED strip configuration")
+print("(a) Strip on (b) Individual LED setup (c) Strip off \n")
+while not exit_loop:
+    if input() == "a" or input() == "A":
+        pixels.fill((0, 255, 0))
+        pixels.show()
+    if input() == "b" or input() == "B":
+        print("Neopixel configuration mode. Entering integers (0-150) will turn on those leds \n")
+        try:
+            start_pix = int(input("Start pixel: "))
+            end_pix = int(input("End pixel: "))
+        except TypeError:
+            print("Could not convert input to integer")
+        else:
+            pixels[start_pix:end_pix] = (0, 255, 0)
+            pixels.show()
+    if input() == "c" or input() == "C":
+        pixels.fill((0, 0, 0))
+        pixels.show()
+
 
 # A note that the lights don't turn off at the end of the script. Will need an off button
+
+
 
 
