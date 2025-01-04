b74d9e98357a3640c477e927a9286f0e2405dfcd
Ali Jones <aliclara564@gmail.com>
2025-01-03 17:56:40 -0800

    modifying led config

--- testled.py
+++ testled.py
@@ -12,12 +12,12 @@ pixels = neopixel.NeoPixel(board.D18, 150, brightness=0.2, auto_write=False)
 exit_loop = False
 
 print("LED strip configuration")
-print("(a) Strip on (b) Individual LED setup (c) Strip off \n")
+print("(a) Strip on (b) Individual LED setup (c) Strip off (q) Quit")
 while not exit_loop:
     if input() == "a" or input() == "A":
         pixels.fill((0, 255, 0))
         pixels.show()
-    if input() == "b" or input() == "B":
+    elif input() == "b" or input() == "B":
         print("Neopixel configuration mode. Entering integers (0-150) will turn on those leds \n")
         try:
             start_pix = int(input("Start pixel: "))
@@ -27,9 +27,11 @@ while not exit_loop:
         else:
             pixels[start_pix:end_pix] = (0, 255, 0)
             pixels.show()
-    if input() == "c" or input() == "C":
+    elif input() == "c" or input() == "C":
         pixels.fill((0, 0, 0))
         pixels.show()
+    elif input() == "q" or input == "Q":
+        exit_loop == True
 
 
 # A note that the lights don't turn off at the end of the script. Will need an off button
