9dc2af0fc7bbed928aadce9e0b636145407376e5
Ali Jones <aliclara564@gmail.com>
2025-01-03 17:58:34 -0800

    forgot how input() works

--- testled.py
+++ testled.py
@@ -12,12 +12,12 @@ pixels = neopixel.NeoPixel(board.D18, 150, brightness=0.2, auto_write=False)
 exit_loop = False
 
 print("LED strip configuration")
-print("(a) Strip on (b) Individual LED setup (c) Strip off (q) Quit")
 while not exit_loop:
-    if input() == "a" or input() == "A":
+    entry = input("(a) Strip on (b) Individual LED setup (c) Strip off (q) Quit")
+    if entry == "a" or entry == "A":
         pixels.fill((0, 255, 0))
         pixels.show()
-    elif input() == "b" or input() == "B":
+    elif entry == "b" or entry == "B":
         print("Neopixel configuration mode. Entering integers (0-150) will turn on those leds \n")
         try:
             start_pix = int(input("Start pixel: "))
@@ -27,10 +27,10 @@ while not exit_loop:
         else:
             pixels[start_pix:end_pix] = (0, 255, 0)
             pixels.show()
-    elif input() == "c" or input() == "C":
+    elif entry == "c" or entry == "C":
         pixels.fill((0, 0, 0))
         pixels.show()
-    elif input() == "q" or input == "Q":
+    elif entry == "q" or entry == "Q":
         exit_loop == True
 
 
