d7c416e4f784dbe7f1038ad70ffa130b67e7d765
rickascii <github@danemea.de>
2025-05-27 16:25:51 -0500

    Completed randomizer indexing system
    Added functionality to extract all ingredients satisfying a given tag into a list.
    
    Added "Sweet Liqueur" as a tag within the liqueur file, and both tags_wine and tags_bitters.

--- randomizer/Playground.py
+++ randomizer/Playground.py
@@ -13,14 +13,40 @@ os.chdir("..\\config")
 
 #What I need to do now is load everything into the document
 
+#This holds in the data of all of the tag files.
+##It's possible that we're going to to run into problems with the aliases, so I might need to exclude that one specifically.
+yamls = {}
 for filename in os.listdir():
-    if filename[-4:] == '.yml':
-        input(filename[:-4]+"\n")
+    if filename[-4:] == '.yml' and filename[:5] == "tags_":
         with open(filename, 'r') as file:
-            eval(filename[:-4] + " = " + "%s"%yaml.safe_load(file))
+            yamls[filename[:-4]] = yaml.safe_load(file)
             file.close()
-        input("Read succesful...\n")
-        print(eval(filename[:-4]))
-        input("And the write too!\n")
-#print(tags_brandy)
 
+#We'd like lists of all tags and ingredients
+tags = []
+ingredients = []
+for tagfile in yamls:
+    for tag in yamls[tagfile].keys():
+        tags.append(tag)
+with open("Ingredients.csv", 'r') as file:
+    data = file.read().split("\n")
+    for i in data:
+        if len(i)>1:
+            ingredients.append(i.split(',')[0])
+
+
+#Armed with the tags and the ingredients, we now
+def get_ingredients(tag):
+    ingredience = []
+    #To avoid recursion
+    investigated_tags = [tag]
+    for tagfile in yamls:
+        if tag in yamls[tagfile]:
+            subsidiaries = yamls[tagfile][tag]['ingredients'].keys()
+            for sub in subsidiaries:
+                if sub in ingredients:
+                    ingredience.append(sub)
+                if sub in tags and sub not in investigated_tags:
+                    ingredience = ingredience + get_ingredients(sub)
+                    investigated_tags.append(sub)
+    return ingredience
