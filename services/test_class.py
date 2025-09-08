32e34d5d5e2c24eb3d3e68e1707679403774a5ab
Ali Jones <aliclara564@gmail.com>
2025-09-07 22:11:43 -0700

    preview and adding w flash works in simulation

--- services/test_class.py
+++ services/test_class.py
@@ -35,7 +35,11 @@ class TestView(FlaskView):
         self.lights = LED()
         self.lit_up_ingredients = []
         self.random_ten = []
-        self.multiprocess = multiprocessing.Process()
+        # self.multiprocess = multiprocessing.Process()
+
+
+        self.input_spirit = ""
+        self.input_coord = ""
 
     def _load_menu(self, verbose=True):
         # Read in the main menu and validate it against our master list of ingredients
@@ -43,7 +47,7 @@ class TestView(FlaskView):
         self.all_ingredients, self.location_dict = recipe.load_all_ingredients()
         self.menu_dict = recipe.validate_all_recipes(menu_dict_raw, self.all_ingredients, self.location_dict, self.tags_dict, alias_dict)
         self.all_ingredients_user_facing = [ingredient.replace("_", " ").title() for ingredient in self.all_ingredients]
-        
+        self.cabinet_locs = recipe.load_cabinet_locs().keys()
         
         if verbose:
             print("--")
@@ -352,8 +356,7 @@ class TestView(FlaskView):
         return render_template('put_away_ingredient.html', ingredients=self.all_ingredients,
                                ingredientSelected=ingredient_selected, locationSelected=location_selected)
       
-
-    
+ 
     @method("GET")
     @method("POST")
     def modify_spirits(self):
@@ -361,16 +364,12 @@ class TestView(FlaskView):
         self._quick_update()
 
         # Set some initial parameters to pass to html
-        add_spirits_disabled = "true"
-        input_spirit = ""
-        input_coord = ""
         recipe_result = ""
         remove_result = ""
         add_result = ""
 
         if request.method == "POST":
             # Clear the LEDS, if they're on
-            # self.lights.forbid_flashing() # this is in all_off()
             self.lights.all_off()
 
             if "input_recipe_name" in request.form.keys():
@@ -394,51 +393,44 @@ class TestView(FlaskView):
                 else:
                     recipe_result = f"Failed to add {updated_name}. Sure would be great if we had logs published to the website"
 
-            elif "input_add_spirit" in request.form.keys():
-                # "Preview" mode
-                if "btn_preview" in request.form.keys():
-                    print("preview spirit mode")
-                    # Get the values of the html input elements
-                    spirit_to_add = request.form["input_add_spirit"]
-                    coord_to_add = request.form["input_add_coord"].upper()
-                    # Check that the given coord is valid
-                    cabinet_locs = recipe.load_cabinet_locs()
-                    if coord_to_add in cabinet_locs.keys():
-                        # If it's valid, light up the pixels. Todo -- need to make this flash
-                        # Not currently threading, I just thought it was
-                        # THink about multiprocessing?
-                        # self.lights.illuminate_location(coord_to_add, flash=True)
-
-                        # self._start_flash(coord_to_add)
-                        self.lights._allow_flashing()
-                        self.lights.illuminate_location(coord_to_add, flash=True)
-
-                        # Update the html display
-                        input_spirit = spirit_to_add
-                        input_coord = coord_to_add
-                        add_spirits_disabled = "false"
-                    else:
-                        print("Invalid coordinate given")
-                # "Add" mode
-                elif "btn_add" in request.form.keys():
-                    print("add spirit mode")
-                    # Get the values of the html input elements
-                    spirit_to_add = request.form["input_add_spirit"]
-                    coord_to_add = request.form["input_add_coord"]
-                    # We've already checked that the coord is valid and we're happy with the location, so we can
-                    # directly update the csv.
-                    # HTML todo -- disable the "Add" button whenever we type in the input form
-                    result = recipe.add_spirit(spirit_to_add, coord_to_add)
+            # "Preview" mode
+            elif "btn_preview" in request.form.keys():
+                print("preview spirit mode")
+                print(request.form)
+                # Get the values of the html input elements
+                self.input_spirit = request.form["input_prev_spirit"]
+                self.input_coord = request.form["input_prev_coord"].upper()
+                # Check that the given coord is valid
+                if self.input_coord in self.cabinet_locs:
+                    # If it's valid, light up the pixels.
+                    # Not currently threading, I just thought it was
+                    # Multiprocessing breaks things
+                    # Add thread from here?
+                    self.lights._allow_flashing()
+                    self.lights.illuminate_location(self.input_coord, flash=True)
+                    add_result = f'Lighting up coordinate {self.input_coord}' # Doesn't currently update (threading problem)
+                else:
+                    add_result = f'Invalid coordinate "{self.input_coord}"'
+            # "Add" mode
+            elif "btn_add" in request.form.keys():
+                print("add spirit mode")
+                print(self.input_spirit)
+                # Since the 'Add' button is always active (because my threading isn't working, that's a todo), do another
+                # coordinate validity check
+                if self.input_coord in self.cabinet_locs:
+                    # If we're good, try to update the CSV
+                    result = recipe.add_spirit(self.input_spirit, self.input_coord)
                     if result:
-                        add_result = f"Successfully added {spirit_to_add} to inventory"
+                        add_result = f"Successfully added {self.input_spirit} to inventory"
                     else:
-                        add_result = f"Failed to add {spirit_to_add}. Is your coordinate valid?"
+                        add_result = f"Failed to add {self.input_spirit} despite a valid coordinate. Hmm."
+                else:
+                    add_result = f'Invalid coordinate "{self.input_coord}"'
 
-                    # Update the html display
-                    input_spirit = ""
-                    input_coord = ""
-                    add_spirits_disabled = "true"
-
+                # Update the html display
+                self.input_spirit = ""
+                self.input_coord = ""
+            # "Remove" mode
             elif "input_remove_spirit" in request.form.keys():
                 print("remove spirit mode")
                 spirit_to_remove = request.form["input_remove_spirit"]
@@ -452,9 +444,9 @@ class TestView(FlaskView):
                     remove_result = f"Failed to remove {spirit_to_remove}. Does that spirit exist?"
 
 
-        return render_template('modify_spirits.html', addSpiritsDisabled=add_spirits_disabled, collections=self.collection_names,
-                               inputSpirit=input_spirit, inputCoord=input_coord, spiritList=self.all_ingredients_user_facing, 
-                               recipeResultString=recipe_result, removeResultString=remove_result, )
+        return render_template('modify_spirits.html', collections=self.collection_names,
+                               inputSpirit=self.input_spirit, inputCoord=self.input_coord, spiritList=self.all_ingredients_user_facing, 
+                               recipeResultString=recipe_result, removeResultString=remove_result, addResultString=add_result)
 
 
 
